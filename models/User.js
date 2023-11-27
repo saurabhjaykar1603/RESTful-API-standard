90521b254cd425a2d33a55b48febcc2067e280f5
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-27 11:52:48 +0530

    ref : User schma fields

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const userSchema = new Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  age: { type: Number, required: true },
+  mobile: { type: String, required: true },
+  adress: { type: String, required: true },
+  gender: ["male", "female", "other"],
 });
 
 const User = model("User", userSchema);
