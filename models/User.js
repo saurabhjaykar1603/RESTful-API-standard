6fd2bcd31a00b3c3deb2a35f2edcd792ef31b9a3
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-27 12:56:33 +0530

    ref : user schema

--- models/User.js
+++ models/User.js
@@ -1,13 +1,14 @@
 import { Schema, model } from "mongoose";
-const userSchema = new Schema({
-  userName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  age: { type: Number, required: true },
-  mobile: { type: String, required: true },
-  adress: { type: String, required: true },
-  gender: ["male", "female", "other"],
-});
+const userSchema = new Schema(
+  {
+    userName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 export default User;
